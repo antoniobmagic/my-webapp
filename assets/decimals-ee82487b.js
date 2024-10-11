d1fd07bfc919a59e45997b116ea33d6e91693721
antoniobmagic <168420686+antoniobmagic@users.noreply.github.com>
2024-10-11 11:02:21 +0000

    Deploying to gh-pages from @ antoniobmagic/my-webapp@cbc4827bce575127876468df4a8d4895103e1330 🚀

---
+++ assets/decimals-ee82487b.js
@@ -0,0 +1,1 @@
+import{cz as a}from"./index-465a7585.js";import{d as e}from"./decimals-51f54468.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
---
+++ assets/sign-1d23b52e.js
@@ -0,0 +1,1 @@
+import{v as n}from"./session-request-b7ed79c3.js";import"./index-465a7585.js";async function o(e){const{account:s,params:a}=e;return n(s,a[1]),s.signMessage({message:{raw:a[0]}})}export{o as handleSignRequest};
